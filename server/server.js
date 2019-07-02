94885745bf2ff9951b68d308cf4a5940cc0023aa
virendra <virendrasingha110@gmail.com>
2019-07-02 09:29:01 +0530

    2 commit

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const path=require('path');
 const publicpath=path.join(__dirname+'./../public');
 const express=require('express');
 const hbs=require('hbs');
+const port=process.env.PORT||3000;
 var app=express();
 app.use(express.static(publicpath));
 app.set('view engine','hbs');
 
-app.listen(3000,()=>{
-	console.log('Started app om port 3000');
+app.listen(port,()=>{
+	console.log(`Started app on port ${port}`);
 });
