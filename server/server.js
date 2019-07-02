9e298fd5eb7a318eb7ac2c34fc2c298bcbfd2ab0
virendra <virendrasingha110@gmail.com>
2019-07-02 19:29:07 +0530

    Made connections between front end and backend using socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const path=require('path');
 const publicpath=path.join(__dirname+'./../public');
 const express=require('express');
+const socketio=require('socket.io');
+const http=require('http');
 const hbs=require('hbs');
 const port=process.env.PORT||3000;
 var app=express();
+var server=http.createServer(app);
+var io=socketio(server);
 app.use(express.static(publicpath));
-app.set('view engine','hbs');
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+	console.log('new user connected');
+	socket.on('disconnect',()=>{
+		console.log('User disconeected');
+	})
+});
+
+server.listen(port,()=>{
 	console.log(`Started app on port ${port}`);
 });
